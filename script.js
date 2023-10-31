32e575689e14097795bd991fed0ba50f8fcae7bf
Mohit Kumar <109014039+Mohitkumar521@users.noreply.github.com>
2023-10-31 15:35:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 function volume_sphere() {
-  var radius = document.getElementById('radius').value;
-  var volume = (4/3) * Math.PI * Math.pow(radius, 3);
-  document.getElementById('volume').value = volume.toFixed(2);
-} 
+  // Get the input element with id "radius" and retrieve its value
+  const radiusInput = document.getElementById('radius');
+  const radius = parseFloat(radiusInput.value);
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+  // Calculate the volume of the sphere
+  const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+  // Get the output element with id "volume" and set its value to the calculated volume
+  const volumeOutput = document.getElementById('volume');
+  volumeOutput.value = volume.toFixed(2); // You can adjust the number of decimal places as needed
+
+  // Prevent the form from submitting and refreshing the page
+  return false;
+}
+
+// This code will be executed when the page is loaded
+window.onload = function() {
+  // Attach the volume_sphere function to the form's submit event
+  document.getElementById('MyForm').onsubmit = volume_sphere;
+};
+
